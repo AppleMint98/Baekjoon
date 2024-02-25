31ea1914debae0dbb782522f8228e45fea26d44d
SeongHoHong <141808008+AppleMint98@users.noreply.github.com>
2024-02-25 15:07:56 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * N가지 자연수 중에서 중복 O M개를 고른 수열
+ * 1 <= m <= n <= 8
+ * 비내림차순
+ */
+
+int n, m;
+int arr[10];
+bool isUsed[10];
+
+void func(int k, int nArray[]) {
+
+    if (k==m) {
+        for (int i = 0; i < m; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if(arr[k-1] <= nArray[i]){
+            arr[k] = nArray[i];
+            isUsed[i] = true;
+            func(k + 1, nArray);
+            isUsed[i] = false;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n >> m;
+    int nArray[n+1] = {0};
+    for (int i = 1; i <= n; i++) {
+        cin >> nArray[i];
+    }
+    sort(nArray + 1, nArray + (n+1));
+    func(0, nArray);
+}
