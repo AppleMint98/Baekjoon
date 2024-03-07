c7d7d3070e27fd13d291e45b1dea090102f756d0
SeongHoHong <141808008+AppleMint98@users.noreply.github.com>
2024-03-07 22:49:00 +0900

    [Bronze IV] Title: 줄번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    cin.ignore();
+    for (int i = 1; i <= n; i++) {
+        string str;
+        getline(cin, str);
+        cout << i << ". " << str << '\n';
+    }
+
+}
