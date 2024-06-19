db5ed33223d69e584779522af3b825babb108bf8
SeongHoHong <141808008+AppleMint98@users.noreply.github.com>
2024-06-19 21:47:40 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string str;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> str;
+
+    cout << ":fan::fan::fan:" << '\n';
+    cout << ":fan::" << str << "::fan:" << '\n';
+    cout << ":fan::fan::fan:";
+
+    return 0;
+}
