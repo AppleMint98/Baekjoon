d03c8cb5770d6b995534f35d5ce6eeacbc25d2d0
SeongHoHong <141808008+AppleMint98@users.noreply.github.com>
2024-06-20 16:29:58 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    if (n == 1) cout << "Leading the Way to the Future";
+    else  cout << "YONSEI"; 
+    
+    return 0;
+}
